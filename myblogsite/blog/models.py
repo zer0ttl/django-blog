73ad64f7db3df8f7fcaf2a5fae65e0e189d74bfd
zer0ttl <sudheer.shirsath+zer0ttl@gmail.com>
2018-04-13 11:53:39 +0530

    home page layout template completed. blog post model initialized with custom user model. static files load completed.

--- myblogsite/blog/models.py
+++ myblogsite/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        (5, 'Draft'),
+        (10, 'Published')
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default=5)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
--- myblogsite/myblogsite/settings.py
+++ myblogsite/myblogsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'myblogsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
+
+# User Authentication
+
+AUTH_USER_MODEL = 'blog.User' # because we are using a custom model
